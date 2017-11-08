12f93297bbff4df38c5a60de9ec13eefa558eda6
GrzegorzOkon <grzegorzokon@gmail.com>
2017-11-08 10:41:59 +0100

    Wprowadzono osobne wątki dla przycisku Wyszukaj w centrali.

--- src/kontroler/Kontroler.java
+++ src/kontroler/Kontroler.java
@@ -33,85 +33,101 @@ public class Kontroler {
 	}
 	
 	public void wyszukajWCentraliNrAkt(ArrayList<String> numeryAkt) {
-		ArrayList<Dokument> dokumenty = new ArrayList<>();
-		
-		for (String numerAkt : numeryAkt) {
-			Dokument dokument = new Dokument(Identyfikator.NUMER_AKT, numerAkt);
-			dokumenty.add(dokument);
-			
-			try {
-				List<DokumentZCentralaCntrValidDok> dokumentyZTabeliCntrValidDok = model.findByNrAktInCntrValidDok(numerAkt);
-				dokument.setDokumentyZCentralaCntrValidDok(dokumentyZTabeliCntrValidDok);
-			
-				//TEST
-				//widok.wyœwietlRaport("wykonano select po numerze akt\n");
+		Thread watek = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				ArrayList<Dokument> dokumenty = new ArrayList<>();
 				
-				if (dokumentyZTabeliCntrValidDok != null) {				
-					for (DokumentZCentralaCntrValidDok dokumentZTabeliCntrValidDok : dokumentyZTabeliCntrValidDok) {
-						if (dokumentZTabeliCntrValidDok.getIdentyfikatorDokumentu() != null) {						
-							DokumentZCentralaDokumenty dokumentZTabeliDokumenty = model.findByIdDokInDokumenty(dokumentZTabeliCntrValidDok.getIdentyfikatorDokumentu());
-							dokument.setDokumentZCentralaDokumenty(dokumentZTabeliDokumenty);	
-							
-							//TEST
-							//widok.wyœwietlRaport(dokumentZTabeliDokumenty.getIdentyfikatorDokumentu() + "\n");
-							//widok.wyœwietlRaport(dokumentZTabeliDokumenty.getSymbolDokumentu() + "\n");
+				for (String numerAkt : numeryAkt) {
+					Dokument dokument = new Dokument(Identyfikator.NUMER_AKT, numerAkt);
+					dokumenty.add(dokument);
+					
+					try {
+						List<DokumentZCentralaCntrValidDok> dokumentyZTabeliCntrValidDok = model.findByNrAktInCntrValidDok(numerAkt);
+						dokument.setDokumentyZCentralaCntrValidDok(dokumentyZTabeliCntrValidDok);
+					
+						//TEST
+						//widok.wyœwietlRaport("wykonano select po numerze akt\n");
+						
+						if (dokumentyZTabeliCntrValidDok != null) {				
+							for (DokumentZCentralaCntrValidDok dokumentZTabeliCntrValidDok : dokumentyZTabeliCntrValidDok) {
+								if (dokumentZTabeliCntrValidDok.getIdentyfikatorDokumentu() != null) {						
+									DokumentZCentralaDokumenty dokumentZTabeliDokumenty = model.findByIdDokInDokumenty(dokumentZTabeliCntrValidDok.getIdentyfikatorDokumentu());
+									dokument.setDokumentZCentralaDokumenty(dokumentZTabeliDokumenty);	
+									
+									//TEST
+									//widok.wyœwietlRaport(dokumentZTabeliDokumenty.getIdentyfikatorDokumentu() + "\n");
+									//widok.wyœwietlRaport(dokumentZTabeliDokumenty.getSymbolDokumentu() + "\n");
+								}
+							}				
 						}
-					}				
+					} catch (Exception ex) {
+						widok.wyœwietlKomunikatB³edu();
+						break;  //wyœwietla raz komunikat b³êdu dla listy komunikatów
+					}	
 				}
-			} catch (Exception ex) {
-				widok.wyœwietlKomunikatB³edu();
-				break;  //wyœwietla raz komunikat b³êdu dla listy komunikatów
-			}	
-		}
+				
+				// Wyœwietla odpowiedni raport
+				model.generujRaporty(dokumenty);
+			}
+		});
 		
-		// Wyœwietla odpowiedni raport
-		model.generujRaporty(dokumenty);
+		watek.start();
 	}
 	
 	//
 	public void wyszukajWCentraliIdDok(ArrayList<String> idDoki) {
-		ArrayList<Dokument> dokumenty = new ArrayList<>();
+		Thread watek = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				ArrayList<Dokument> dokumenty = new ArrayList<>();
 		
-		for (String identyfikatorDokumentu : idDoki) {
-			Dokument dokument = new Dokument(Identyfikator.IDENTYFIKATOR_DOKUMENTU, identyfikatorDokumentu);
-			dokumenty.add(dokument);
+				for (String identyfikatorDokumentu : idDoki) {
+					Dokument dokument = new Dokument(Identyfikator.IDENTYFIKATOR_DOKUMENTU, identyfikatorDokumentu);
+					dokumenty.add(dokument);
 			
-			try {
-				DokumentZCentralaDokumenty dokumentZTabeliDokumenty = model.findByIdDokInDokumenty(identyfikatorDokumentu);					
+					try {
+						DokumentZCentralaDokumenty dokumentZTabeliDokumenty = model.findByIdDokInDokumenty(identyfikatorDokumentu);					
 				
-				//TEST
-				//widok.wyœwietlRaport("" + dokumentZTabeliDokumenty.getIdentyfikatorDokumentu());
+						//TEST
+						//widok.wyœwietlRaport("" + dokumentZTabeliDokumenty.getIdentyfikatorDokumentu());
 				
-				dokument.setDokumentZCentralaDokumenty(dokumentZTabeliDokumenty);
+						dokument.setDokumentZCentralaDokumenty(dokumentZTabeliDokumenty);
 				
-				if (dokumentZTabeliDokumenty == null) {
-					//TEST
-					//widok.wyœwietlRaport("brak w dokumentach\n");
+						if (dokumentZTabeliDokumenty == null) {
+							//TEST
+							//widok.wyœwietlRaport("brak w dokumentach\n");
 					
-					DokumentZCentralaCntrValidDok dokumentZTabeliCntrValidDok = model.findByIdDokInCntrValidDok(identyfikatorDokumentu);
-					dokument.setDokumentyZCentralaCntrValidDok(dokumentZTabeliCntrValidDok);
+							DokumentZCentralaCntrValidDok dokumentZTabeliCntrValidDok = model.findByIdDokInCntrValidDok(identyfikatorDokumentu);
+							dokument.setDokumentyZCentralaCntrValidDok(dokumentZTabeliCntrValidDok);
 					
-					//TEST
-					/*if (dokumentZTabeliCntrValidDok == null) {
-						widok.wyœwietlRaport("brak w cntr_valid_dok\n");
-					} else {
-						widok.wyœwietlRaport(dokumentZTabeliCntrValidDok.getIdentyfikatorDokumentu());
-					}*/
-				} 
-				//TEST
-				//else {
-					//TEST
-					//widok.wyœwietlRaport(dokument.getDokumentZCentralaDokumenty().getIdentyfikatorDokumentu() + "\n");
-				//}
+							//TEST
+							/*if (dokumentZTabeliCntrValidDok == null) {
+								widok.wyœwietlRaport("brak w cntr_valid_dok\n");
+							} else {
+								widok.wyœwietlRaport(dokumentZTabeliCntrValidDok.getIdentyfikatorDokumentu());
+							}*/
+						} 
+						//TEST
+						//else {
+						//TEST
+						//widok.wyœwietlRaport(dokument.getDokumentZCentralaDokumenty().getIdentyfikatorDokumentu() + "\n");
+						//}
 				
-			} catch (Exception ex) {
-				widok.wyœwietlKomunikatB³edu();
-				break;  //wyœwietla raz komunikat b³êdu dla listy komunikatów
-			}		
-		}
+					} catch (Exception ex) {
+						widok.wyœwietlKomunikatB³edu();
+						break;  //wyœwietla raz komunikat b³êdu dla listy komunikatów
+					}		
+				}
 		
-		// Wyœwietla odpowiedni raport
-		model.generujRaporty(dokumenty);
+				// Wyœwietla odpowiedni raport
+				model.generujRaporty(dokumenty);
+			}
+		});
+		
+		watek.start();
 	}
 	
     /**
@@ -121,32 +137,31 @@ public class Kontroler {
      * 			 Lista numerów symboli dokumentów po których nastapi wyszukiwanie w bazie.
      */ 
 	public void wyszukajWCentraliSymDok(ArrayList<String> symDok) {
-		ArrayList<Dokument> dokumenty = new ArrayList<>();
-		
-		for (String symbolDokumentu : symDok) {
-			Dokument dokument = new Dokument(Identyfikator.SYMBOL_DOKUMENTU, symbolDokumentu);
-			dokumenty.add(dokument);
-			
-			try {
-				DokumentZCentralaDokumenty dokumentZTabeliDokumenty = model.findBySymDokInDokumenty(symbolDokumentu);
+		Thread watek = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				ArrayList<Dokument> dokumenty = new ArrayList<>();
 				
-				dokument.setDokumentZCentralaDokumenty(dokumentZTabeliDokumenty);
-			
-				//TEST
-				/*if (dokumentZTabeliDokumenty == null) {
-					//TEST
-					widok.wyœwietlRaport("brak w dokumentach\n");
-				} else {
-					//TEST
-					widok.wyœwietlRaport(dokumentZTabeliDokumenty.getSymbolDokumentu());
-				}*/
-			} catch (Exception ex) {
-				widok.wyœwietlKomunikatB³edu();
-				break;  //wyœwietla raz komunikat b³êdu dla listy komunikatów
+				for (String symbolDokumentu : symDok) {
+					Dokument dokument = new Dokument(Identyfikator.SYMBOL_DOKUMENTU, symbolDokumentu);
+					dokumenty.add(dokument);
+					
+					try {
+						DokumentZCentralaDokumenty dokumentZTabeliDokumenty = model.findBySymDokInDokumenty(symbolDokumentu);
+						
+						dokument.setDokumentZCentralaDokumenty(dokumentZTabeliDokumenty);
+					} catch (Exception ex) {
+						widok.wyœwietlKomunikatB³edu();
+						break;  //wyœwietla raz komunikat b³êdu dla listy komunikatów
+					}
+				}
+				
+				// Wyœwietla odpowiedni raport
+				model.generujRaporty(dokumenty);
 			}
-		}
+		});
 		
-		// Wyœwietla odpowiedni raport
-		model.generujRaporty(dokumenty);
+		watek.start();
 	}
 }
