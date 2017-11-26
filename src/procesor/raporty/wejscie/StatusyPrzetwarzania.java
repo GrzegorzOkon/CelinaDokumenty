d3525568f060b3626ba28c5b45306cfadd77b01f
GrzegorzOkon <grzegorzokon@gmail.com>
2017-11-26 21:19:30 +0100

    Dodałem opisy statusów dokumentów PWD2 w tabeli dokumenty w klasie StatusyPrzetwarzania.

--- src/procesor/raporty/wejscie/StatusyPrzetwarzania.java
+++ src/procesor/raporty/wejscie/StatusyPrzetwarzania.java
@@ -1,11 +1,16 @@
 package procesor.raporty.wejscie;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
+/**
+ * Klasa inicjalizuj¹ca i zwracaj¹ca opisy statusów dokumentów dla przes³anych symboli.
+ * 
+ * @author Grzegorz Okoñ
+ */
 public class StatusyPrzetwarzania {
 	private static StatusyPrzetwarzania instancja;
 	
+	private Map<String, String> statusyPWD2;
 	private Map<String, String> statusySAD2;
 	
 	public static StatusyPrzetwarzania pobierzInstancjê() {
@@ -17,10 +22,35 @@ public class StatusyPrzetwarzania {
 	
 	public String pobierzStatus(String rodzajDokumentu, String statusPrzetwarzania) {
 		switch(rodzajDokumentu) {
+			case "PWD2" : return pobierzStatusPWD2(statusPrzetwarzania);
 			case "SAD2" : return pobierzStatusSAD2(statusPrzetwarzania);
 			
 			default : return null;
 		}
+	}
+	
+	private String pobierzStatusPWD2(String statusPrzetwarzania) {
+		if (statusyPWD2 == null) {
+			inicjalizujStatusyPWD2();
+		} 	
+		
+		return statusyPWD2.get(statusPrzetwarzania);
+	}
+	
+	private void inicjalizujStatusyPWD2() {
+		statusyPWD2 = new HashMap<>();
+		
+		statusyPWD2.put("F", "po awarii");
+		statusyPWD2.put("G", "po kontroli");
+		statusyPWD2.put("K", "uniewa¿niony");
+		statusyPWD2.put("N", "do wyjaœnienia");
+		statusyPWD2.put("O", "w rejestracji");
+		statusyPWD2.put("Q", "odrzucony");
+		statusyPWD2.put("R", "oczekuj¹cy");
+		statusyPWD2.put("T", "dla prawnika");
+		statusyPWD2.put("U", "anulowany");
+		statusyPWD2.put("X", "zamkniêty");
+		statusyPWD2.put("Y", "przyjêty");
 	}
 	
 	private String pobierzStatusSAD2(String statusPrzetwarzania) {
@@ -91,8 +121,6 @@ public class StatusyPrzetwarzania {
 	private void inicjalizujStatusyPW() {}
 	
 	private void inicjalizujStatusyPWD() {}
-		
-	private void inicjalizujStatusyPWD2() {}
 		
 	private void inicjalizujStatusyPZC() {}
 
